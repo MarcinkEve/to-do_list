2a55b37b6bdf9f477afa3578c7e2236d9f701792
MarcinkEve <marcinke@gmail.com>
2022-07-16 18:28:30 +0300

    "Form" component edit form input "value" and "onChange" valuues added

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 
-const Form = ({newTask, setNewTask, addTask}) => {
+const Form = ({ newTask, setNewTask, addTask, updateData, changeTask }) => {
   return (
     <div>
       <form>
-        <input type="text" name="toDo" id="toDo" placeholder="enter task" />
+        <input
+          value={updateData && updateData.title}
+          onChange={changeTask}
+          type="text"
+          name="toDo"
+          id="toDo"
+          placeholder="enter task"
+        />
         <button type="update">Update</button>
         <button type="submit">Cancel</button>
       </form>
       <form>
-        <input value={newTask} onChange={(event)=>setNewTask(event.target.value)} type="text" name="toDo" id="toDo" placeholder="enter task" />
-        <button onClick={addTask} type="submit">Submit</button>
+        <input
+          value={newTask}
+          onChange={(event) => setNewTask(event.target.value)}
+          type="text"
+          name="toDo"
+          id="toDo"
+          placeholder="enter task"
+        />
+        <button onClick={addTask} type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
