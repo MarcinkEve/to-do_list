73c1ab7c57ded5d63bff37b01896b37c998f37e3
MarcinkEve <marcinke@gmail.com>
2022-07-16 18:52:16 +0300

    "FOr" component: edit form is hidden (if "pencil" icon is clicked, then edit form is shown)

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -10,35 +10,42 @@ const Form = ({
 }) => {
   return (
     <div>
-      {/* Update record */}
-      <form>
-        <input
-          value={updateData && updateData.title}
-          onChange={changeTask}
-          type="text"
-          name="toDo"
-          id="toDo"
-          placeholder="enter task"
-        />
-        <button onClick={updateTask} type="update">
-          Update
-        </button>
-        <button type="submit">Cancel</button>
-      </form>
-      {/* New record */}
-      <form>
-        <input
-          value={newTask}
-          onChange={(event) => setNewTask(event.target.value)}
-          type="text"
-          name="toDo"
-          id="toDo"
-          placeholder="enter task"
-        />
-        <button onClick={addTask} type="submit">
-          Submit
-        </button>
-      </form>
+      {updateData ? (
+        <>
+          {/* Update record */}
+          <form>
+            <input
+              value={updateData && updateData.title}
+              onChange={changeTask}
+              type="text"
+              name="toDo"
+              id="toDo"
+              placeholder="enter task"
+            />
+            <button onClick={updateTask} type="update">
+              Update
+            </button>
+            <button type="submit">Cancel</button>
+          </form>
+        </>
+      ) : (
+        <>
+          {/* New record */}
+          <form>
+            <input
+              value={newTask}
+              onChange={(event) => setNewTask(event.target.value)}
+              type="text"
+              name="toDo"
+              id="toDo"
+              placeholder="enter task"
+            />
+            <button onClick={addTask} type="submit">
+              Submit
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
