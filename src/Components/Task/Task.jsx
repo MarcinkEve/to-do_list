8198fc74716cc0e2e504f0d19b02b2dd7c78427a
MarcinkEve <marcinke@gmail.com>
2022-07-16 17:09:11 +0300

    Icons (done, edit, delete) for "Task" component inserted

--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./task.css";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { TiPencil } from "react-icons/ti";
+import { GiTrashCan } from "react-icons/gi";
 
 const List = ({ task, index }) => {
   return (
-    <div>
-      <div className="taskBg">
-        <div className={task.status ? "done" : ""}>
-          <span className="taskNumber">{index + 1}</span>
-          <span className="taskText">{task.title}</span>
-        </div>
+    <div className="taskBg">
+      <div className={task.status ? "done" : ""}>
+        <span className="taskNumber">{index + 1}</span>
+        <div className="taskText">{task.title}</div>
+      </div>
+      <div className="iconsWrap">
+        <AiOutlineCheckCircle title="Done/Undone" />
+        <TiPencil title="Edit" />
+        <GiTrashCan title="Delete" />
       </div>
     </div>
   );
