3a406f2096ff5cba117e0dbefd34518013189e92
MarcinkEve <marcinke@gmail.com>
2022-07-16 15:08:37 +0300

    to do list task state created with test values (at end point there will be an empty array)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./Components/Form/Form";
 
 function App() {
+  const [toDo, setToDo] = useState([
+    { id: 1, title: "Task-1", status: false },
+    { id: 1, title: "Task-2", status: false },
+  ]);
+
   return (
     <div className="App">
       <header className="App-header">
         Here will be To Do List...
+        <Form />
       </header>
     </div>
   );
