181a24e4ebd9f760708f464784df3a5bb51895c8
MarcinkEve <marcinke@gmail.com>
2022-07-19 19:16:37 +0300

    Form for task editing put to "EditForm" component

---
+++ src/Components/EditForm/EditForm.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
+
+const EditForm = ({
+  newTask,
+  newWhen,
+  setNewWhen,
+  setNewTask,
+  addTask,
+  updateData,
+  cancelUpdate,
+  changeHolder,
+  changeHolderWhen,
+  updateTask,
+  when,
+}) => {
+  return (
+    <>
+      {/* Update record */}
+      <form className="form">
+        <input
+          value={updateData && updateData.title}
+          // onChange={(e) => setNewTask(e.target.value)}
+          onChange={(event) => changeHolder(event)}
+          type="text"
+          name="toDo"
+          id="toDo"
+          placeholder="Add updated task"
+          className="form-input"
+        />
+        <select
+          value={updateData.when}
+          // onChange={(e) => setNewWhen(e.target.value)}
+          onChange={(event) => changeHolderWhen(event)}
+          className="form-select"
+        >
+          {when.map((el, i) => (
+            <option key={i}>{el}</option>
+          ))}
+        </select>
+        <button onClick={updateTask} type="update" className="form-button">
+          <AiOutlineCheck className="form-button--icon-edit" />
+        </button>
+        <button onClick={cancelUpdate} type="submit" className="form-button">
+          <AiOutlineClose className="form-button--icon-cancel" />
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default EditForm;
